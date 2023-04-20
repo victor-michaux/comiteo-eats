70b301df462bdc819c582a6724c69e7820060bf1
Louis nicolas <uknowcreation@gmail.com>
2023-04-20 13:43:36 +0200

    feat(router): create routes associated to pages

--- assets/router/index.js
+++ assets/router/index.js
@@ -1,9 +1,35 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import Home from '../pages/Home';
+import Restaurant from '../pages/Restaurant';
+import Cart from '../pages/Cart';
 
 Vue.use(VueRouter);
 
-const routes = [];
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: Home,
+    },
+    {
+        path: '/restaurant/:id',
+        name: 'restaurant',
+        component: Restaurant,
+        props: true,
+    },
+    {
+        path: '/cart',
+        name: 'cart',
+        component: Cart,
+        props: true,
+    },
+    {
+        path: '*',
+        name: 'not-found',
+        component: () => import('../pages/404.vue'),
+    },
+];
 
 const router = new VueRouter({
     mode: 'history',
