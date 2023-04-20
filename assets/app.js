7f8146dcfaa167792c0cc80e3bee277a2c041cb2
Louis nicolas <uknowcreation@gmail.com>
2023-04-20 13:40:17 +0200

    feat(app): importing store font and entry point tailwind css

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,24 @@
 import Vue from 'vue';
-import InstantSearch from 'vue-instantsearch';
-import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import router from './router';
 import store from './store';
 
-import './styles.css';
+import App from './App.vue';
+import Header from './components/Layout/Header';
 
-Vue.use(InstantSearch);
+import './css/main.css';
+
+Vue.component('Header', Header);
+Vue.component('FontAwesomeIcon', FontAwesomeIcon);
+library.add(fas);
+
+Vue.config.productionTip = false;
 
 new Vue({
+    library,
+    FontAwesomeIcon,
     router,
     store,
     render: (h) => h(App),
