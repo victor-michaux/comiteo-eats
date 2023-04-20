1a2bc53a698f7dcaf5dbe498cc0109a21985c723
Louis nicolas <uknowcreation@gmail.com>
2023-04-20 13:43:18 +0200

    feat(pages): group pages by using types

---
+++ assets/pages/Cart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container mx-auto px-4 my-5">
+        <Checkout />
+    </div>
+</template>
+<script>
+import Checkout from '../components/shared/cart/Checkout';
+
+export default {
+    name: 'Cart',
+    components: {
+        Checkout,
+    },
+};
+</script>
---
+++ assets/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div class="container mx-auto px-5">
+            <food-category />
+            <hr class="h-px bg-gray border-0" />
+            <restaurant-list />
+        </div>
+    </div>
+</template>
+
+<script>
+import FoodCategory from '../components/shared/foodCategory/FoodCategory';
+import RestaurantList from '../components/shared/restaurant/RestaurantList';
+
+export default {
+    name: 'Home',
+    components: {
+        RestaurantList,
+        FoodCategory,
+    },
+};
+</script>
